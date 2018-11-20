ed19ef88e8943c6a6111a0756baae10eb74220a3
InteJonny <42732575+InteJonny@users.noreply.github.com>
2018-11-20 09:15:23 +0100

    Create uppgiftfive.java
    uppgift 4

---
+++ uppgiftfive.java
@@ -0,0 +1,17 @@
+package programering;
+
+public class uppgiftfive {
+
+	public static void main(String[] args) {
+		
+		String xchar = "x";
+		
+		for (int x = 1; x <= 4; x++) {
+			
+			System.out.println(xchar);
+			
+			xchar = xchar + "x";
+		}
+	}
+	
+}
